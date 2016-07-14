0128a6a3310a81b9907e2b95c724b1701b07ff63
tabradford <tabradford@liberty.edu>
2016-07-14 12:55:35 -0400

    Added some basic page to test concept of app.UseDefaultFiles() and app.UseStaticFiles()...

--- Learning/src/Learning/Startup.cs
+++ Learning/src/Learning/Startup.cs
@@ -28,10 +28,12 @@ namespace Learning
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
