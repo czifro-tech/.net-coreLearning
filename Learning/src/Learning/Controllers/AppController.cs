698a6c6896b7eb29ff37056ad14b70dac0700ed4
tabradford <tabradford@liberty.edu>
2016-07-15 14:57:29 -0400

    Moved _ViewStart to the correct folder. Added New routes.

--- Learning/src/Learning/Controllers/AppController.cs
+++ Learning/src/Learning/Controllers/AppController.cs
@@ -4,11 +4,21 @@ using Microsoft.AspNetCore.Mvc;
 namespace Learning.Controllers
 {
 
-        public class AppController : Controller
+    public class AppController : Controller
     {
-            public IActionResult Index() 
-            {
-                return View();
-            }
+        public IActionResult Index()
+        {
+            return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult ValueReturn()
+        {
+            return View();
+        }
+    }
 }
